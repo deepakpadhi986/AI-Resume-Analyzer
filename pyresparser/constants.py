5a1082bd0fd2746e3a49b681ff12c02f2b951098
Deepak Padhi <81186601+deepakpadhi986@users.noreply.github.com>
2022-09-26 21:49:21 +0530

    Customized Pyresparser files
    Copy the code of this file with respect to Pyresparser file (both have same names so no confusion)

---
+++ pyresparser/constants.py
@@ -0,0 +1,55 @@
+from nltk.corpus import stopwords
+
+
+NAME_PATTERN = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+
+# Education (Upper Case Mandatory)
+EDUCATION = [
+            'BE', 'B.E.', 'B.E', 'BS', 'B.S', 'ME', 'M.E',
+            'M.E.', 'MS', 'M.S', 'BTECH', 'MTECH',
+            'SSC', 'HSC', 'CBSE', 'ICSE', 'X', 'XII'
+        ]
+
+NOT_ALPHA_NUMERIC = r'[^a-zA-Z\d]'
+
+NUMBER = r'\d+'
+
+# For finding date ranges
+MONTHS_SHORT = r'''(jan)|(feb)|(mar)|(apr)|(may)|(jun)|(jul)
+                   |(aug)|(sep)|(oct)|(nov)|(dec)'''
+MONTHS_LONG = r'''(january)|(february)|(march)|(april)|(may)|(june)|(july)|
+                   (august)|(september)|(october)|(november)|(december)'''
+MONTH = r'(' + MONTHS_SHORT + r'|' + MONTHS_LONG + r')'
+YEAR = r'(((20|19)(\d{2})))'
+
+STOPWORDS = set(stopwords.words('english'))
+
+RESUME_SECTIONS_PROFESSIONAL = [
+                    'experience',
+                    'education',
+                    'interests',
+                    'professional experience',
+                    'publications',
+                    'skills',
+                    'certifications',
+                    'objective',
+                    'career objective',
+                    'summary',
+                    'leadership'
+                ]
+
+RESUME_SECTIONS_GRAD = [
+                    'accomplishments',
+                    'experience',
+                    'education',
+                    'interests',
+                    'projects',
+                    'professional experience',
+                    'publications',
+                    'skills',
+                    'certifications',
+                    'objective',
+                    'career objective',
+                    'summary',
+                    'leadership'
+                ]
---
+++ pyresparser/resume_parser.py
@@ -0,0 +1,151 @@
+
+
+import os
+import multiprocessing as mp
+import io
+import spacy
+import pprint
+from spacy.matcher import Matcher
+from . import utils
+
+
+class ResumeParser(object):
+
+    def __init__(
+        self,
+        resume,
+        skills_file=None,
+        custom_regex=None
+    ):
+        nlp = spacy.load('en_core_web_sm')
+        custom_nlp = spacy.load(os.path.dirname(os.path.abspath(__file__)))
+        self.__skills_file = skills_file
+        self.__custom_regex = custom_regex
+        self.__matcher = Matcher(nlp.vocab)
+        self.__details = {
+            'name': None,
+            'email': None,
+            'mobile_number': None,
+            'skills': None,
+            'college_name': None,
+            'degree': None,
+            'designation': None,
+            'experience': None,
+            'company_names': None,
+            'no_of_pages': None,
+            'total_experience': None,
+        }
+        self.__resume = resume
+        if not isinstance(self.__resume, io.BytesIO):
+            ext = os.path.splitext(self.__resume)[1].split('.')[1]
+        else:
+            ext = self.__resume.name.split('.')[1]
+        self.__text_raw = utils.extract_text(self.__resume, '.' + ext)
+        self.__text = ' '.join(self.__text_raw.split())
+        self.__nlp = nlp(self.__text)
+        self.__custom_nlp = custom_nlp(self.__text_raw)
+        self.__noun_chunks = list(self.__nlp.noun_chunks)
+        self.__get_basic_details()
+
+    def get_extracted_data(self):
+        return self.__details
+
+    def __get_basic_details(self):
+        cust_ent = utils.extract_entities_wih_custom_model(
+                            self.__custom_nlp
+                        )
+        name = utils.extract_name(self.__nlp, matcher=self.__matcher)
+        email = utils.extract_email(self.__text)
+        mobile = utils.extract_mobile_number(self.__text, self.__custom_regex)
+        skills = utils.extract_skills(
+                    self.__nlp,
+                    self.__noun_chunks,
+                    self.__skills_file
+                )
+        # edu = utils.extract_education(
+        #               [sent.string.strip() for sent in self.__nlp.sents]
+        #       )
+        entities = utils.extract_entity_sections_grad(self.__text_raw)
+
+        # extract name
+        try:
+            self.__details['name'] = cust_ent['Name'][0]
+        except (IndexError, KeyError):
+            self.__details['name'] = name
+
+        # extract email
+        self.__details['email'] = email
+
+        # extract mobile number
+        self.__details['mobile_number'] = mobile
+
+        # extract skills
+        self.__details['skills'] = skills
+
+        # extract college name
+        try:
+            self.__details['college_name'] = entities['College Name']
+        except KeyError:
+            pass
+
+        # extract education Degree
+        try:
+            self.__details['degree'] = cust_ent['Degree']
+        except KeyError:
+            pass
+
+        # extract designation
+        try:
+            self.__details['designation'] = cust_ent['Designation']
+        except KeyError:
+            pass
+
+        # extract company names
+        try:
+            self.__details['company_names'] = cust_ent['Companies worked at']
+        except KeyError:
+            pass
+
+        try:
+            self.__details['experience'] = entities['experience']
+            try:
+                exp = round(
+                    utils.get_total_experience(entities['experience']) / 12,
+                    2
+                )
+                self.__details['total_experience'] = exp
+            except KeyError:
+                self.__details['total_experience'] = 0
+        except KeyError:
+            self.__details['total_experience'] = 0
+        self.__details['no_of_pages'] = utils.get_number_of_pages(
+                                            self.__resume
+                                        )
+        return
+
+
+def resume_result_wrapper(resume):
+    parser = ResumeParser(resume)
+    return parser.get_extracted_data()
+
+
+if __name__ == '__main__':
+    pool = mp.Pool(mp.cpu_count())
+
+    resumes = []
+    data = []
+    for root, directories, filenames in os.walk('resumes'):
+        for filename in filenames:
+            file = os.path.join(root, filename)
+            resumes.append(file)
+
+    results = [
+        pool.apply_async(
+            resume_result_wrapper,
+            args=(x,)
+        ) for x in resumes
+    ]
+
+    results = [p.get() for p in results]
+
+    pprint.pprint(results)
